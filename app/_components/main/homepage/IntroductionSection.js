658fc6705df62da0431d7662e93e344071b3d425
Tamar86 <takkokikalishvili@gmail.com>
2025-02-03 20:54:58 +0000

    Changes in introduction section in font size

--- app/_components/main/homepage/IntroductionSection.js
+++ app/_components/main/homepage/IntroductionSection.js
@@ -1,7 +1,7 @@
 function IntroductionSection() {
 	return (
 		<div className='flex flex-col gap-24 items-center justify-center px-6'>
-			<h1 className='text-5xl font-extrabold leading-snug text-center max-w-3xl'>
+			<h1 className='text-4xl md:text-5xl font-extrabold leading-snug text-center max-w-3xl'>
 				<span className='text-blue-500'>Hi, I&apos;m Tamara.</span>
 				<br />I design and develop <span className='text-blue-400'>modern</span>
 				,<span className='text-green-400'> responsive</span>, and
