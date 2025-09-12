cee62e26a55eafacd5d840467f7c5b19bbe694b1
Tamara K.C <takkokikalishvili@gmail.com>
2025-09-12 14:40:09 +0100

    RESUME ADDED

--- app/utils/myProjects.js
+++ app/utils/myProjects.js
@@ -2,33 +2,40 @@ export const myProjects = [
 	{
 		id: 1,
 		technologies: '',
+		title: 'Internal Portal Demo (CRM)',
+		image: '/portal-demo.png',
+		link: 'https://internal-portal-demo.vercel.app',
+	},
+	{
+		id: 2,
+		technologies: '',
 		title: 'Rock paper scissors game',
 		image: '/paper-rock-scissors.png',
 		link: 'https://rock-paper-scissors-game-sooty-omega.vercel.app/',
 	},
 	{
-		id: 2,
+		id: 3,
 		technologies: '',
 		title: 'Space tourism multi page website',
 		image: '/space.png',
 		link: 'https://space-tourism-multi-page-website-teal.vercel.app/',
 	},
 	{
-		id: 3,
+		id: 4,
 		technologies: '',
 		title: 'Todo app with color theme switcher',
 		image: '/to-do.png',
 		link: 'https://tamar86.github.io/todo-app/',
 	},
 	{
-		id: 4,
+		id: 5,
 		technologies: '',
 		title: 'rest countries api with color theme switcher',
 		image: '/api-countries.png',
 		link: 'https://tamar86.github.io/rest-countries-api-with-color-theme-switcher/',
 	},
 	{
-		id: 5,
+		id: 6,
 		technologies: '',
 		title: 'Mortgage Calculator',
 		image: '/mortgage-calculator.png',
@@ -36,7 +43,7 @@ export const myProjects = [
 	},
 
 	{
-		id: 6,
+		id: 7,
 		technologies: '',
 		title: 'product list with cart',
 		image: '/cart.png',
